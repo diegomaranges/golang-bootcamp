f51247b9f27cb4b0f6b03b7d2d56a5afc4318a4b
Diego Martin Maranges <diego.maranges@globant.com>
2020-01-22 10:53:14 -0300

    first example

--- main.go
+++ main.go
@@ -3,5 +3,26 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Printf("hello, world\n")
+	var option string
+	var imput string
+	var sliceWithElements []string
+
+	for {
+		fmt.Println(sliceWithElements)
+
+		lengt, err := fmt.Scanf("%s", &option)
+
+		if lengt == 1 && err == nil {
+			_, err := fmt.Scanf("%s\n", &imput)
+
+			if err == nil {
+				fmt.Println(imput)
+				sliceWithElements = append(sliceWithElements, imput)
+			}
+
+		} else {
+			fmt.Println(err)
+			fmt.Println(lengt)
+		}
+	}
 }
