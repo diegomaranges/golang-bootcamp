6cb11798faa958f4c2fc7f500b020e516ca35d1a
Diego Martin Maranges <diego.maranges@globant.com>
2020-01-22 15:52:53 -0300

    implement reed input from default format

--- main.go
+++ main.go
@@ -1,28 +1,75 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+var sliceWithElements []string
 
 func main() {
 	var option string
-	var imput string
-	var sliceWithElements []string
+	var inputS string
+	inputType := bufio.NewScanner(os.Stdin)
 
 	for {
-		fmt.Println(sliceWithElements)
 
-		lengt, err := fmt.Scanf("%s", &option)
+		fmt.Println("select an option:")
+		fmt.Println("add     -> for add a new element")
+		fmt.Println("retrive -> for show all elements")
+		fmt.Println("update  -> for replace an element")
+		fmt.Println("delete  -> for delete an element")
+		fmt.Print(">>")
 
-		if lengt == 1 && err == nil {
-			_, err := fmt.Scanf("%s\n", &imput)
+		inputType.Scan()
+		option = inputType.Text()
+		option = strings.ToLower(option)
 
-			if err == nil {
-				fmt.Println(imput)
-				sliceWithElements = append(sliceWithElements, imput)
-			}
+		switch option {
+		case "add":
+			fmt.Println("write the new element")
+			fmt.Print(">>")
+			inputType.Scan()
+			inputS = inputType.Text()
+			addElement(inputS)
 
-		} else {
-			fmt.Println(err)
-			fmt.Println(lengt)
+		case "retrive":
+			showElements()
+
+		case "update":
+			fmt.Println("update")
+			fmt.Println("")
+
+		case "delete":
+			inputType.Scan()
+			inputS = inputType.Text()
+			addElement(inputS)
+			fmt.Println("")
+
+		default:
+			fmt.Println("not valid option")
+			fmt.Println("")
 		}
+
 	}
+}
+
+func addElement(element string) {
+	sliceWithElements = append(sliceWithElements, element)
+	fmt.Println("")
+}
+
+func deleteElement(element string) {
+
+}
+
+func showElements() {
+	fmt.Println("************************")
+	for _, element := range sliceWithElements {
+		fmt.Println(element)
+	}
+	fmt.Println("************************")
+	fmt.Println("")
 }
