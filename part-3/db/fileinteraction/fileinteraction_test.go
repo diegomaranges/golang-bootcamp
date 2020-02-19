8dcf238cadd5ec789309f0357ba5ab83d196db0b
Diego Martin Maranges <diego.maranges@globant.com>
2020-02-19 18:28:54 -0300

    update fileinteraction test

--- part-3/db/fileinteraction/fileinteraction_test.go
+++ part-3/db/fileinteraction/fileinteraction_test.go
@@ -11,15 +11,20 @@ func TestOne(t *testing.T) {
 	file := CreateNewFInstance()
 	var mapExample map[string]*Items
 	mapExample = make(map[string]*Items)
+	var quantity int
 
 	/*Set destiny file*/
-	file.SetFile("text")
+	file.SetFile("db")
 	resultDestiny := file.ReturnDestinyFile()
-	assert.Equal(t, resultDestiny, "text.json", "error to set a destiny file")
+	assert.Equal(t, resultDestiny, "db.json", "Error to set a destiny file")
 
 	/*Read and load empty file*/
 	err := file.ReadFile(mapExample)
-	assert.NoError(t, err, "Error to read and load a empty file")
+	assert.Equal(t, len(mapExample), 3, "Error to read and load a non-empty file")
+	assert.NoError(t, err, "Error to read and load a non-empty file")
+	quantity = mapExample["1"].Quantity
+
+	mapExample["1"].Quantity++
 
 	/*write 2 elements in the file*/
 	err = file.WriteFile(mapExample)
@@ -27,6 +32,8 @@ func TestOne(t *testing.T) {
 
 	/*Read and load non-empty file*/
 	err = file.ReadFile(mapExample)
+	assert.Equal(t, len(mapExample), 3, "Error to read and load a non-empty file")
+	assert.Equal(t, mapExample["1"].Quantity, quantity+1, "Error to write the map into the file")
 	assert.NoError(t, err, "Error to read and load a non-empty file")
 
 	err = file.WriteFile(mapExample)
