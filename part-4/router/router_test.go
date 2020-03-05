41a92c4559f396a96cace3fcd12d2747e92230a7
Diego Martin Maranges <diego.maranges@globant.com>
2020-03-05 11:33:31 -0300

    refactor: router test

--- part-4/router/router_test.go
+++ part-4/router/router_test.go
@@ -6,7 +6,45 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func generateRoutesTest(t *testing.T) {
+func TestNewRoute(t *testing.T) {
 	newRouter := NewRoute()
 	assert.NotNil(t, newRouter, "error with create route (length)")
+
+	/*expected values*/
+	tables := []struct {
+		name string
+	}{
+		{
+			name: "SignIn",
+		},
+		{
+			name: "Refresh",
+		},
+		{
+			name: "CreateNewCar",
+		},
+		{
+			name: "getSpecificCar",
+		},
+		{
+			name: "DeleteCar",
+		},
+		{
+			name: "getSpecificItem",
+		},
+		{
+			name: "addItem",
+		},
+		{
+			name: "updateItem",
+		},
+		{
+			name: "deleteItem",
+		},
+	}
+
+	for _, table := range tables {
+		myRoute := newRouter.GetRoute(table.name)
+		assert.NotNil(t, myRoute, "error: Url does not created")
+	}
 }
